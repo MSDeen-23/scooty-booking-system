5b2ad8e823eeb85777760c5e35da3c70c1ccf848
sharuk <sharuk@beezlabs.com>
2022-06-19 10:29:26 +0530

    Init commit

---
+++ src/test/java/com/example/scootybookingsystem/ScootyBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scootybookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScootyBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
